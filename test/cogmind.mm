f74c71e7d6a79fbb7418bb9d61bbd8601b1bf845
Ingy dot Net <ingy@ingydotnet1.(none)>
2010-01-05 12:12:32 -0800

    Add ToDo and a test mm

---
+++ test/cogmind.mm
@@ -0,0 +1,15 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1262718956396" ID="Freemind_Link_1682095074" MODIFIED="1262721234252" TEXT="cogmind">
+<node CREATED="1262721241361" ID="_" MODIFIED="1262721297710" POSITION="right" TEXT="Mashup of CogBase and MindMap"/>
+<node CREATED="1262721308209" ID="Freemind_Link_496786864" MODIFIED="1262721313082" POSITION="right" TEXT="CogBase"/>
+<node CREATED="1262721314598" ID="Freemind_Link_504338998" MODIFIED="1262721319850" POSITION="right" TEXT="FreeMind"/>
+<node CREATED="1262721329667" ID="Freemind_Link_758429664" MODIFIED="1262721333627" POSITION="right" TEXT="Hackers">
+<node CREATED="1262721320833" ID="Freemind_Link_307446053" MODIFIED="1262721328736" TEXT="Alex"/>
+<node CREATED="1262721338209" ID="Freemind_Link_1766908926" MODIFIED="1262721340286" TEXT="Ingy"/>
+</node>
+<node CREATED="1262721379070" ID="Freemind_Link_1841100081" MODIFIED="1262721383996" POSITION="right" TEXT="Technology">
+<node CREATED="1262721298814" ID="Freemind_Link_963712744" MODIFIED="1262721307217" TEXT="Python"/>
+</node>
+</node>
+</map>
